2e04afb146519993109fe4aab47c89f3a79a304c
Razvan <razwan.me@gmail.com>
2021-06-11 14:29:29 +0300

    Output advanced color palettes config to window.styleManager.colorsConfig

--- src/Provider/GeneralAssets.php
+++ src/Provider/GeneralAssets.php
@@ -46,6 +46,7 @@ class GeneralAssets extends AbstractHookProvider {
 	 */
 	public function register_hooks() {
 		$this->add_action( 'init', 'register_assets', 1 );
+		$this->add_action( 'wp_print_scripts', 'print_inline_scripts', 1 );
 	}
 
 	/**
@@ -69,6 +70,18 @@ class GeneralAssets extends AbstractHookProvider {
 				sm_get_palette_output_from_color_config( $advanced_palettes_output )
 			);
 		}
+	}
 
+	protected function print_inline_scripts() {
+		$advanced_palettes_output = $this->options->get( 'sm_advanced_palette_output' );
+
+		ob_start(); ?>
+
+		<script id="style-manager-colors-config">
+			window.styleManager = window.styleManager || {};
+			window.styleManager.colorsConfig = JSON.parse( <?php echo json_encode( $advanced_palettes_output ); ?> );
+		</script>
+
+		<?php echo ob_get_clean();
 	}
 }
