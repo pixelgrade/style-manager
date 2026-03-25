3f2dedea28a6c6d2b8344982005ef74c5f21e54e
George Olaru <george@pixelgrade.com>
2026-03-25 12:47:33 +0200

    Remove active radio-group checkmark icon
    Fixes #71

--- tests/js/radio-group-style.test.js
+++ tests/js/radio-group-style.test.js
@@ -4,10 +4,16 @@ export const runRadioGroupStyleTests = async ( assert ) => {
   const { readFile } = await import( 'node:fs/promises' );
   const source = await readFile( RADIO_GROUP_SCSS_PATH, 'utf8' );
   const labelBlockMatch = source.match( /label\s*\{([\s\S]*?)&:nth-of-type\(2\)/ );
+  const checkedLabelBlockMatch = source.match( /&:checked\s*\+\s*label\s*\{([\s\S]*?)\n\s*\}\n\s*\}/ );
 
   assert.ok( labelBlockMatch, 'expected to find the shared sm-radio-group label rule' );
+  assert.ok( checkedLabelBlockMatch, 'expected to find the shared checked label rule' );
   assert.ok(
     /transition\s*:\s*all\s+1s\s+ease\s*;/.test( labelBlockMatch[1] ),
     'shared sm-radio-group labels should use the temporary 1s inspection transition'
+  );
+  assert.ok(
+    ! /&:before\s*\{/.test( checkedLabelBlockMatch[1] ),
+    'shared sm-radio-group active labels should not render a checkmark pseudo-element'
   );
 };
