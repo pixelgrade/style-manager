32e1f8cfb2ef978807552edec0ecbc59ee5dc4c8
George Olaru <george@pixelgrade.com>
2026-03-25 12:20:05 +0200

    Remove radio-group transition lag in Customizer controls
    Fixes #67

---
+++ tests/js/radio-group-style.test.js
@@ -0,0 +1,13 @@
+const RADIO_GROUP_SCSS_PATH = new URL( '../../src/_js/customizer/scss/controls/_radio-group.scss', import.meta.url );
+
+export const runRadioGroupStyleTests = async ( assert ) => {
+  const { readFile } = await import( 'node:fs/promises' );
+  const source = await readFile( RADIO_GROUP_SCSS_PATH, 'utf8' );
+  const labelBlockMatch = source.match( /label\s*\{([\s\S]*?)&:nth-of-type\(2\)/ );
+
+  assert.ok( labelBlockMatch, 'expected to find the shared sm-radio-group label rule' );
+  assert.ok(
+    ! /transition\s*:/.test( labelBlockMatch[1] ),
+    'shared sm-radio-group labels should not animate state changes'
+  );
+};
